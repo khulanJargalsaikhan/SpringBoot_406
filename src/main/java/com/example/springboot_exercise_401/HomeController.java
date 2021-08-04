75aaabf8b2b8c71c6340e0d24a7e663adb58f9de
khulanJargalsaikhan <onagraa@gmail.com>
2021-08-04 09:25:11 -0400

    SpringBoot - Security-405

--- src/main/java/com/example/springboot_exercise_401/HomeController.java
+++ src/main/java/com/example/springboot_exercise_401/HomeController.java
@@ -1,10 +1,25 @@
 package com.example.springboot_exercise_401;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    //principal is currently logged in
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model){
+        String username = principal.getName();
+        model.addAttribute("user", userRepository.findByUsername(username));
+        return "secure";
+    }
 
     @RequestMapping("/")
     public String homepage(){
--- src/main/java/com/example/springboot_exercise_401/UserRepository.java
+++ src/main/java/com/example/springboot_exercise_401/UserRepository.java
@@ -3,6 +3,6 @@ package com.example.springboot_exercise_401;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+    User findByUsername(String username);
 
 }
